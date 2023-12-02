27beef6d13e04158e9dea99818f10df6f8e11f3f
Zaherha <z1_ahmad@hotmail.com>
2023-12-02 14:49:37 +0300

    NEW: Player rotation multiplayer

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,24 +1,25 @@
 using Arrows;
 using FMODUnity;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace Player {
-    public class PlayerController : MonoBehaviour {
+    public class PlayerController : NetworkBehaviour {
+        [SerializeField] private float networkInterpolationSpeed = .1f;
         [SerializeField] private float lookSpeed = 3;
-        [SerializeField] private float verticalClamp = 45;
+        [SerializeField] private float cameraVerticalClamp = 90;
 
-        [SerializeField] private Transform mainCamera;
-        
         [SerializeField] private Arrow defaultArrow;
         [SerializeField] private Transform arrowSpawnPoint;
-        
+
         [SerializeField] private Transform bow;
         [SerializeField] private float maxBowMovement = 0.4f;
 
         [SerializeField] private StudioEventEmitter releaseSound;
         [SerializeField] private StudioEventEmitter drawSound;
-        
+
         [SerializeField] private PlayerTeleportObject currentPlayerTeleportObject;
+        [SerializeField] private Camera playerCamera;
 
         private Arrow arrow;
         private float draw;
@@ -26,30 +27,55 @@ namespace Player {
         private float initialYRotation;
         private Vector2 rotation = Vector2.zero;
 
+        private readonly NetworkVariable<Vector2> networkRotation = new();
+        private readonly NetworkVariable<Vector3> networkEulerAngle = new();
+        private readonly NetworkVariable<Quaternion> networkCameraRotation = new();
+
         public PlayerTeleportObject CurrentPlayerTeleportObject {
             get => currentPlayerTeleportObject;
             set => currentPlayerTeleportObject = value;
         }
 
         private void Start() {
-            initialYRotation = transform.eulerAngles.y;
+            if (!IsOwner) {
+                playerCamera.enabled = false;
+            }
         }
 
         public void Update() {
-            LookUpdate();
-            DrawUpdate();
+            if (IsOwner) {
+                HandleLookUpdateServerRPC(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), rotation);
+            }
+
+
+            rotation = networkRotation.Value;
+            transform.rotation = Quaternion.Lerp(Quaternion.Euler(transform.eulerAngles), Quaternion.Euler(networkEulerAngle.Value), networkInterpolationSpeed);
+            playerCamera.transform.localRotation = Quaternion.Lerp(playerCamera.transform.localRotation, networkCameraRotation.Value, networkInterpolationSpeed);
         }
 
-        private void LookUpdate() {
-            float yChange = Input.GetAxis("Mouse Y") * lookSpeed;
-            rotation.x -= yChange;
-            rotation.x = Mathf.Clamp(rotation.x, -verticalClamp, verticalClamp);
+        [ServerRpc(RequireOwnership = false)]
+        private void HandleLookUpdateServerRPC(float inputX, float inputY, Vector2 rotation) {
+            LookUpdate(inputX, inputY, rotation);
+        }
 
-            float xChange = Input.GetAxis("Mouse X") * lookSpeed;
+        private void LookUpdate(float inputX, float inputY, Vector2 rotation) {
+            float yChange = inputY * lookSpeed;
+            rotation.x -= yChange;
+            rotation.x = Mathf.Clamp(rotation.x, -cameraVerticalClamp, cameraVerticalClamp);
+
+            float xChange = inputX * lookSpeed;
             rotation.y += xChange;
+            networkRotation.Value = rotation;
+
+            networkEulerAngle.Value = new Vector2(0, rotation.y + initialYRotation);
+            networkCameraRotation.Value = Quaternion.Euler(rotation.x, 0, 0);
+        }
 
             transform.eulerAngles = new Vector2(0, rotation.y + initialYRotation);
             mainCamera.transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+        [ServerRpc(RequireOwnership = false)]
+        private void HandleDrawUpdateServerRPC(bool draw) {
+            DrawUpdate(draw);
         }
         
         private void DrawUpdate() {
@@ -75,11 +101,6 @@ namespace Player {
                 arrow = null;
                 draw = 0;
                 releaseSound.Play();
-            }
-
-            // Reset the bow position if its not already reset
-            if (bow.localPosition.z != 0f) {
-                bow.localPosition = Vector3.Lerp(bow.localPosition, Vector3.zero, Time.deltaTime * 10f);
             }
         }
     }
