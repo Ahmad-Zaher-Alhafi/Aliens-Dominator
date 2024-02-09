17f5dfb6925afd300bed6752b2eb7abd3bd6cb26
Zaherha <z1_ahmad@hotmail.com>
2024-02-09 22:28:26 +0300

    FIX: Automation commander was not synced on the network properly

--- Assets/Scripts/SecurityWeapons/AutomationCommander.cs
+++ Assets/Scripts/SecurityWeapons/AutomationCommander.cs
@@ -1,29 +1,48 @@
 using FiniteStateMachine;
+using Multiplayer;
 using TMPro;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace SecurityWeapons {
-    public class AutomationCommander : MonoBehaviour {
+    public class AutomationCommander : NetworkBehaviour {
         [SerializeField] private TextMeshProUGUI activationStateText;
 
         private IAutomatable automatableToCommand;
         private GameObject lastArrowHitWith;
-
+        private readonly NetworkVariable<SerializedNetworkString> status = new(new SerializedNetworkString(""));
 
         public void Init(IAutomatable automatableToCommand) {
             this.automatableToCommand = automatableToCommand;
+        }
+
+        private void Update() {
             SetActivationStateText();
         }
 
         private void OnTriggerEnter(Collider other) {
             if (!other.gameObject.CompareTag(Constants.Arrow) || other.gameObject == lastArrowHitWith) return;
             lastArrowHitWith = other.gameObject;
+
+            if (IsServer) {
+                automatableToCommand.IsAutomatingEnabled = !automatableToCommand.IsAutomatingEnabled;
+            } else {
+                ChangeAutomationStatusServerRPC();
+            }
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void ChangeAutomationStatusServerRPC() {
             automatableToCommand.IsAutomatingEnabled = !automatableToCommand.IsAutomatingEnabled;
-            SetActivationStateText();
         }
 
         private void SetActivationStateText() {
-            activationStateText.text = automatableToCommand.IsAutomatingEnabled ? "Enabled" : "Disabled";
+            if (IsServer) {
+                activationStateText.text = automatableToCommand.IsAutomatingEnabled ? "Enabled" : "Disabled";
+                status.Value = new SerializedNetworkString(activationStateText.text);
+            } else {
+                activationStateText.text = status.Value.Value;
+            }
         }
     }
 }
