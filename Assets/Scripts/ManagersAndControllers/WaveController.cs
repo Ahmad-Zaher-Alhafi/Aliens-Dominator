d938d63e36d3d0783c0bd63dfa52d631a4fcd28b
Zaherha <z1_ahmad@hotmail.com>
2024-03-08 21:16:08 +0300

    FIX: Path drawers not being cleared until the wave is finished
    i have also moved the draw logic from the controller to the path drawer itself which made the controller much cleaner

--- Assets/Scripts/ManagersAndControllers/WaveController.cs
+++ Assets/Scripts/ManagersAndControllers/WaveController.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Context;
 using Multiplayer;
 using ScriptableObjects;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.AI;
 using Utils;
 
 namespace ManagersAndControllers {
@@ -24,15 +22,13 @@ namespace ManagersAndControllers {
         [Header("Path drawer")]
         [SerializeField] private PathDrawer pathDrawerPrefab;
         [SerializeField] private Transform pathDrawersHolder;
-        [SerializeField] private float fragmentationDistance = 3;
-        [SerializeField] private float pathCreationSpeed = .03f;
 
         public int WavesCount => waves.Count;
         public Wave CurrentWave => waves[CurrentWaveIndex];
-        
+
         public int CurrentWaveIndex => currentWaveIndex;
         private int currentWaveIndex = -1;
-        
+
         public int NextWaveIndex => currentWaveIndex + 1;
         public bool HasWaveStarted { get; private set; }
 
@@ -81,53 +77,8 @@ namespace ManagersAndControllers {
         }
 
         private void DrawPath(List<Vector3> pathPoints) {
-            LineRenderer pathDrawerLocal = pathDrawerPrefab.GetObject<PathDrawer>(pathDrawersHolder).GetComponent<LineRenderer>();
-            pathDrawerLocal.positionCount = 0;
-
-            List<Vector3> pointsToDraw = new List<Vector3>();
-
-            for (int i = 1; i < pathPoints.Count; i++) {
-                Vector3 previousPoint = pathPoints[i - 1];
-                Vector3 currentPoint = pathPoints[i];
-                Vector3 direction = (currentPoint - previousPoint).normalized;
-
-                NavMeshPath navMeshPath = new NavMeshPath();
-                NavMesh.CalculatePath(previousPoint, currentPoint, NavMesh.AllAreas, navMeshPath);
-
-                for (int j = 1; j < navMeshPath.corners.Length; j++) {
-                    previousPoint = navMeshPath.corners[j - 1];
-                    currentPoint = navMeshPath.corners[j];
-
-                    float distance = Vector3.Distance(previousPoint, currentPoint);
-                    int numOfFragments = Mathf.CeilToInt(distance / fragmentationDistance);
-
-                    float highestY = Mathf.Max(previousPoint.y, currentPoint.y);
-                    float lowestY = Mathf.Min(previousPoint.y, currentPoint.y);
-
-                    for (int k = 0; k <= numOfFragments; k++) {
-                        Vector3 fragmentPoint = Vector3.MoveTowards(previousPoint, currentPoint, k * fragmentationDistance);
-
-                        if (Physics.Raycast(new Vector3(fragmentPoint.x, 1000f, fragmentPoint.z), Vector3.down, out RaycastHit hit, Mathf.Infinity, 1 << 3)) {
-
-                            if (hit.point.y > highestY) {
-                                // If the fragment point y position is higher than the two original points, then make sure that the straight path between those two points is blocked
-                                if (!Physics.Raycast(new Vector3(fragmentPoint.x, fragmentPoint.y + fragmentationDistance, fragmentPoint.z), direction, out RaycastHit _, fragmentationDistance, 1 << 3)) continue;
-                            }
-
-                            if (hit.point.y < lowestY) {
-                                // If the fragment point y position is lower than the two original points, then make sure that the straight path between those two points is blocked
-                                if (Physics.Raycast(new Vector3(fragmentPoint.x, fragmentPoint.y + fragmentationDistance, fragmentPoint.z), direction, out RaycastHit _, fragmentationDistance, 1 << 3)) continue;
-                            }
-
-                            fragmentPoint.y = hit.point.y + 1;
-                        }
-
-                        pointsToDraw.Add(fragmentPoint);
-                    }
-                }
-            }
-
-            StartCoroutine(DrawPathsSlowly(pathDrawerLocal, pointsToDraw));
+            PathDrawer pathDrawer = pathDrawerPrefab.GetObject<PathDrawer>(pathDrawersHolder);
+            pathDrawer.Draw(pathPoints);
         }
 
         private void DrawPaths(List<List<Vector3>> pathsPoints) {
@@ -140,30 +91,12 @@ namespace ManagersAndControllers {
             }
 
             DrawPathsClientRPC(clientPathsPoints);
-
-            foreach (List<Vector3> pathPoints in pathsPoints) {
-                DrawPath(pathPoints);
-            }
         }
 
         [ClientRpc]
         private void DrawPathsClientRPC(SerializedNetworkVector3List[] pathsPoints) {
             foreach (SerializedNetworkVector3List pathPoints in pathsPoints) {
                 DrawPath(pathPoints.Objects.ToList());
-            }
-        }
-
-        /// <summary>
-        /// Draw it slowly so the player can watch it while being drawn
-        /// </summary>
-        /// <param name="pathDrawerLocal"></param>
-        /// <param name="pointsToDraw"></param>
-        /// <returns></returns>
-        private IEnumerator DrawPathsSlowly(LineRenderer pathDrawerLocal, List<Vector3> pointsToDraw) {
-            for (int i = 0; i < pointsToDraw.Count; i++) {
-                yield return new WaitForSeconds(pathCreationSpeed);
-                pathDrawerLocal.positionCount = i + 1;
-                pathDrawerLocal.SetPosition(i, pointsToDraw[i]);
             }
         }
     }
--- Assets/Scripts/PathDrawer.cs
+++ Assets/Scripts/PathDrawer.cs
@@ -1,13 +1,18 @@
-using Context;
+using System.Collections;
+using System.Collections.Generic;
 using DG.Tweening;
 using Pool;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PathDrawer : PooledObject {
-    [SerializeField] private float animationDuration = .5f;
     [SerializeField] private Material materialPrefab;
     [SerializeField] private LineRenderer linerRenderer;
-    
+    [SerializeField] private float animationDuration = .5f;
+    [SerializeField] private float fragmentationDistance = 3;
+    [SerializeField] private float pathCreationSpeed = .03f;
+    [SerializeField] private float timeToClearPath = 5;
+
     private void Awake() {
         Material material = Instantiate(materialPrefab);
         linerRenderer.material = material;
@@ -16,15 +21,79 @@ public class PathDrawer : PooledObject {
             .OnStepComplete(() => material.mainTextureOffset = Vector2.zero)
             .SetLink(gameObject)
             .Play();
-
-        Ctx.Deps.EventsManager.WaveFinished += OnWaveFinished;
     }
 
-    private void OnWaveFinished() {
+    public void Draw(List<Vector3> pathPoints) {
+        linerRenderer.positionCount = 0;
+
+        List<Vector3> pointsToDraw = new List<Vector3>();
+
+        for (int i = 1; i < pathPoints.Count; i++) {
+            Vector3 previousPoint = pathPoints[i - 1];
+            Vector3 currentPoint = pathPoints[i];
+            Vector3 direction = (currentPoint - previousPoint).normalized;
+
+            NavMeshPath navMeshPath = new NavMeshPath();
+            NavMesh.CalculatePath(previousPoint, currentPoint, NavMesh.AllAreas, navMeshPath);
+
+            for (int j = 1; j < navMeshPath.corners.Length; j++) {
+                previousPoint = navMeshPath.corners[j - 1];
+                currentPoint = navMeshPath.corners[j];
+
+                float distance = Vector3.Distance(previousPoint, currentPoint);
+                int numOfFragments = Mathf.CeilToInt(distance / fragmentationDistance);
+
+                float highestY = Mathf.Max(previousPoint.y, currentPoint.y);
+                float lowestY = Mathf.Min(previousPoint.y, currentPoint.y);
+
+                for (int k = 0; k <= numOfFragments; k++) {
+                    Vector3 fragmentPoint = Vector3.MoveTowards(previousPoint, currentPoint, k * fragmentationDistance);
+
+                    if (Physics.Raycast(new Vector3(fragmentPoint.x, 1000f, fragmentPoint.z), Vector3.down, out RaycastHit hit, Mathf.Infinity, 1 << 3)) {
+
+                        if (hit.point.y > highestY) {
+                            // If the fragment point y position is higher than the two original points, then make sure that the straight path between those two points is blocked
+                            if (!Physics.Raycast(new Vector3(fragmentPoint.x, fragmentPoint.y + fragmentationDistance, fragmentPoint.z), direction, out RaycastHit _, fragmentationDistance, 1 << 3)) continue;
+                        }
+
+                        if (hit.point.y < lowestY) {
+                            // If the fragment point y position is lower than the two original points, then make sure that the straight path between those two points is blocked
+                            if (Physics.Raycast(new Vector3(fragmentPoint.x, fragmentPoint.y + fragmentationDistance, fragmentPoint.z), direction, out RaycastHit _, fragmentationDistance, 1 << 3)) continue;
+                        }
+
+                        fragmentPoint.y = hit.point.y + 1;
+                    }
+
+                    pointsToDraw.Add(fragmentPoint);
+                }
+            }
+        }
+
+        StartCoroutine(DrawPathSlowly(pointsToDraw));
+    }
+
+    /// <summary>
+    /// Draw it slowly so the player can watch it while being drawn
+    /// </summary>
+    /// <param name="pointsToDraw"></param>
+    /// <returns></returns>
+    private IEnumerator DrawPathSlowly(List<Vector3> pointsToDraw) {
+        for (int i = 0; i < pointsToDraw.Count; i++) {
+            yield return new WaitForSeconds(pathCreationSpeed);
+            linerRenderer.positionCount = i + 1;
+            linerRenderer.SetPosition(i, pointsToDraw[i]);
+        }
+
+        StartCoroutine(ClearPathDelayed());
+    }
+
+    private IEnumerator ClearPathDelayed() {
+        yield return new WaitForSeconds(timeToClearPath);
+        Clear();
+    }
+
+    private void Clear() {
+        linerRenderer.positionCount = 0;
         ReturnToPool();
-    }
-
-    private void OnDestroy() {
-        Ctx.Deps.EventsManager.WaveFinished -= OnWaveFinished;
     }
 }
