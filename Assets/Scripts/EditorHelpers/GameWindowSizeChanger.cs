79469a2ded8894aa6e2a6d4528be2268a3c4e16b
Zaherha <z1_ahmad@hotmail.com>
2024-07-06 15:49:53 +0300

    FIX: Build was failing

--- Assets/Scripts/EditorHelpers/GameWindowSizeChanger.cs
+++ Assets/Scripts/EditorHelpers/GameWindowSizeChanger.cs
@@ -1,13 +1,13 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 
 namespace EditorHelpers {
     public class GameWindowSizeChanger : MonoBehaviour {
-#if UNITY_EDITOR
         private void Update() {
             if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Space)) {
                 UnityEditor.EditorWindow.focusedWindow.maximized = !UnityEditor.EditorWindow.focusedWindow.maximized;
             }
-#endif
         }
     }
 }
+#endif
