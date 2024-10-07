adac4e525e0de5b7aa4dfba94267daac3c693c7a
Zaherha <z1_ahmad@hotmail.com>
2024-10-07 14:22:15 +0300

    NEW: View modes control binding buttons on edge of screen
    This will let the player understand that he can switch view modes using that binding key

---
+++ Assets/Scripts/UI/ControlsUI.cs
@@ -0,0 +1,29 @@
+﻿using Context;
+using ManagersAndControllers;
+using UnityEngine;
+
+namespace UI {
+    public class ControlsUI : MonoBehaviour {
+        [SerializeField] private GameObject fpsViewButton;
+        [SerializeField] private GameObject topDownViewButton;
+
+        private void Awake() {
+            Ctx.Deps.EventsManager.ViewModeChanged += OnViewModeChanged;
+        }
+
+        private void OnViewModeChanged(GameController.ViewMode previousViewMode, GameController.ViewMode currentViewMode) {
+            if (currentViewMode == GameController.ViewMode.General) {
+                fpsViewButton.SetActive(false);
+                topDownViewButton.SetActive(false);
+                return;
+            }
+
+            fpsViewButton.SetActive(currentViewMode != GameController.ViewMode.FPS);
+            topDownViewButton.SetActive(currentViewMode != GameController.ViewMode.TopDown);
+        }
+
+        private void OnDestroy() {
+            Ctx.Deps.EventsManager.ViewModeChanged -= OnViewModeChanged;
+        }
+    }
+}
