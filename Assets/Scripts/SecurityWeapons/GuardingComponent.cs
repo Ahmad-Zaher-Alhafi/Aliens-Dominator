99818ca3171041aa100e203a57bf2424df85c999
Zaherha <z1_ahmad@hotmail.com>
2024-10-08 23:10:11 +0300

    REFACTORING: Guarding state now uses GuardingComponent to do the rotation logic while the state is for activation only
    i have a weapon placeholder that is only a mesh that should play guard animation, the logic is same of GuardingState logic, that's why i extracted the logic to the GuardingComponent class and now i can use it on both my actual automated weapon and on my weapon placeholder

---
+++ Assets/Scripts/SecurityWeapons/GuardingComponent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace SecurityWeapons {
+    public class GuardingComponent : MonoBehaviour {
+        [SerializeField] private float guardingSpeed = 10;
+
+        private Quaternion initialRotation;
+        private Vector2 rotateOnXAxisRange;
+        private Vector2 rotateOnYAxisRange;
+        private Action onFinishCallBack;
+        private Quaternion? targetRotation;
+
+        public void Init(Quaternion initialRotation, Vector2 rotateOnXAxisRange, Vector2 rotateOnYAxisRange, Action onFinishCallBack = null) {
+            this.initialRotation = initialRotation;
+            this.rotateOnXAxisRange = rotateOnXAxisRange;
+            this.rotateOnXAxisRange = rotateOnYAxisRange;
+            this.onFinishCallBack = onFinishCallBack;
+        }
+
+
+        // To let the weapon rotates randomly (escorting area)
+        public void Guard() {
+            if (targetRotation == null) {
+                GenerateRandomRotation();
+            }
+
+            if (targetRotation == null) return;
+
+            if (transform.rotation != targetRotation) {
+                // Smoothly rotate towards the target rotation
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation.Value, guardingSpeed * Time.deltaTime);
+            } else {
+                onFinishCallBack?.Invoke();
+                targetRotation = null;
+            }
+        }
+
+        private void GenerateRandomRotation() {
+            Vector3 initialEulerAngels = initialRotation.eulerAngles;
+
+            // Get random angel on X axis
+            Vector3 targetEulerAngels = Vector3.right * (initialEulerAngels.x + Random.Range(rotateOnYAxisRange.x, rotateOnYAxisRange.y));
+            // Get random angel on Y axis
+            targetEulerAngels += Vector3.up * (initialEulerAngels.y + Random.Range(rotateOnXAxisRange.x, rotateOnXAxisRange.y));
+            // Give the same angel on Z axis
+            targetEulerAngels += Vector3.forward * initialRotation.eulerAngles.z;
+
+            // Return it as quaternion to avoid the gimbal lock of the euler angels
+            targetRotation = Quaternion.Euler(targetEulerAngels);
+        }
+    }
+}
