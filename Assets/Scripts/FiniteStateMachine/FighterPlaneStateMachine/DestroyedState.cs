6386779f79603aba6c4b68257d092bd7ab260528
Zaherha <z1_ahmad@hotmail.com>
2024-10-19 00:29:48 +0300

    FIX: DestroyedState for both security weapon and fighter plane state machines were not getting activated

--- Assets/Scripts/FiniteStateMachine/FighterPlaneStateMachine/DestroyedState.cs
+++ Assets/Scripts/FiniteStateMachine/FighterPlaneStateMachine/DestroyedState.cs
@@ -1,10 +1,16 @@
-﻿using SecurityWeapons;
+﻿using Context;
+using SecurityWeapons;
 
 namespace FiniteStateMachine.FighterPlaneStateMachine {
     public class DestroyedState : FighterPlaneState {
         public override FighterPlaneStateType Type => FighterPlaneStateType.Destroyed;
-        public override bool CanBeActivated() => AutomatedObject.IsDestroyed;
+        public override bool CanBeActivated() => AutomatedObject.Health <= 0;
 
         public DestroyedState(FighterPlane fighterPlane, bool checkWhenAutomatingDisabled) : base(fighterPlane, checkWhenAutomatingDisabled) { }
+
+        public override void Activate(bool isSecondaryState = false) {
+            base.Activate(isSecondaryState);
+            Ctx.Deps.ConstructionController.BulldozeWeapon(AutomatedObject);
+        }
     }
 }
--- Assets/Scripts/FiniteStateMachine/SecurityWeaponMachine/DestroyedState.cs
+++ Assets/Scripts/FiniteStateMachine/SecurityWeaponMachine/DestroyedState.cs
@@ -1,10 +1,16 @@
-﻿using SecurityWeapons;
+﻿using Context;
+using SecurityWeapons;
 
 namespace FiniteStateMachine.SecurityWeaponMachine {
     public class DestroyedState<TEnemyType> : SecurityWeaponState<TEnemyType> where TEnemyType : IAutomatable {
         public override SecurityWeaponStateType Type => SecurityWeaponStateType.Destroyed;
-        public override bool CanBeActivated() => false;
+        public override bool CanBeActivated() => AutomatedObject.Health <= 0;
 
         public DestroyedState(SecurityWeapon<TEnemyType> automatedObject, bool checkWhenAutomatingDisabled) : base(automatedObject, checkWhenAutomatingDisabled) { }
+
+        public override void Activate(bool isSecondaryState = false) {
+            base.Activate(isSecondaryState);
+            Ctx.Deps.ConstructionController.BulldozeWeapon(AutomatedObject);
+        }
     }
 }
