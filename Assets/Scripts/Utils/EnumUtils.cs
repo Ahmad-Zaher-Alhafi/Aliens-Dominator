e19e717fd1e1e9ae96033f5a468bdf1a29fdc457
Zaherha <z1_ahmad@hotmail.com>
2024-10-21 16:25:19 +0300

    NEW: Enum utils

---
+++ Assets/Scripts/Utils/EnumUtils.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Utils {
+    public static class EnumUtils {
+        public static T GetRandomEnumValue<T>() where T : Enum {
+            Array values = Enum.GetValues(typeof(T));
+            int randomIndex = UnityEngine.Random.Range(0, values.Length);
+            return (T) values.GetValue(randomIndex);
+        }
+    }
+}
